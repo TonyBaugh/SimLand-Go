7fd129f2adfd38c6eff2de2e89cfdbc0cfb0bb4b
Psychl0ne <tbaugh87@gmail.com>
2026-07-13 20:37:25 -0400

    follow the leader behavior working

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"image/color"
 	"log"
 	"math"
-	"math/rand/v2"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -17,108 +16,78 @@ const (
 	worldHeight = 1080
 )
 
-type Game struct {	
-	particles []Particle
-	RGBA color.RGBA
+type Game struct {
+	particles            []Particle
+	RGBA                 color.RGBA
 	debugColorDifference float64
-}
-
-type Particle struct {
-	xPos float32
-	yPos float32
-	xVel float32
-	yVel float32
-	radius float32
-	particleColor color.RGBA
 }
 
 type RGBA struct {
 	R, G, B, A uint8
 }
 
-func (p *Particle) Update() {
-	p.xPos += p.xVel
-	p.yPos += p.yVel
+func calculateColorDifference(a, b color.RGBA) float64 {
+	redColorA := int(a.R)
+	greenColorA := int(a.G)
+	blueColorA := int(a.B)
 
-	//Wall Collision Detection
-	//Right Wall
-	if p.xPos + p.radius >= worldWidth {
-		p.xPos = worldWidth - p.radius
-		p.xVel *= -1
-	}
-	// Left Wall
-	if p.xPos <= p.radius {
-		p.xPos = p.radius
-		p.xVel *= -1
-	}
-	//Bottom wall
-	if p.yPos + p.radius >= worldHeight {
-		p.yPos = worldHeight - p.radius
-		p.yVel *= -1
-	}
-	//Top wall
-	if p.yPos <= p.radius {
-		p.yPos = p.radius
-		p.yVel *= -1
-	}
-}
+	redColorB := int(b.R)
+	greenColorB := int(b.G)
+	blueColorB := int(b.B)
 
-func createParticles(count int) []Particle {
-	particleList := make([]Particle, 0, count)
-	radius := 3
-		
-	for range count {
-		particle := Particle {
-			xPos: float32(rand.IntN(worldWidth - 2*radius) + radius),
-			yPos: float32(rand.IntN(worldHeight - 2*radius)+ radius),
-			xVel: float32(rand.IntN(10) + 1),
-			yVel: float32(rand.IntN(10) + 1),
-			radius: float32(radius),
-			particleColor: color.RGBA{uint8(rand.IntN(256)), uint8(rand.IntN(256)), uint8(rand.IntN(256)), 255},
-		}
-		particleList = append(particleList, particle)
-	}
-	return particleList
-}
+	redDifference := redColorA - redColorB
+	greenDifference := greenColorA - greenColorB
+	blueDifference := blueColorA - blueColorB
 
-func calculateColorDifference(a, b color.RGBA) (float64) {
-redColorA := int(a.R)
-greenColorA := int(a.G)
-blueColorA := int(a.B)
-
-redColorB := int(b.R)
-greenColorB := int(b.G)
-blueColorB := int(b.B)
-
-redDifference := redColorA - redColorB
-greenDifference := greenColorA - greenColorB
-blueDifference := blueColorA - blueColorB
-
-return math.Abs(float64(redDifference)) + 
-	   math.Abs(float64(greenDifference)) + 
-	   math.Abs(float64(blueDifference))
+	return math.Abs(float64(redDifference)) +
+		math.Abs(float64(greenDifference)) +
+		math.Abs(float64(blueDifference))
 }
 
 func (g *Game) Update() error {
 	g.debugColorDifference = calculateColorDifference(g.particles[0].particleColor, g.particles[1].particleColor)
-	for i := range g.particles{
-		g.particles[i].Update()		
+
+	xDiff, yDiff := particleDistance(int(g.particles[0].xPos), int(g.particles[1].xPos), int(g.particles[0].yPos), int(g.particles[1].yPos))
+
+	log.Printf("xdiff %d, yDiff %d", xDiff, yDiff)
+
+	// Follow the leader
+	//p[0] = red, p[1] = green
+	// if xdiff is positive, red is to the right of green, red needs to move negative(left)
+	if xDiff > 3 {
+		g.particles[0].xVel = -3
+	} else if xDiff < -3{
+		g.particles[0].xVel = 3
+	} else {
+		g.particles[0].xVel = 0
+	}
+
+	// if yDiff is positive red is below green, needs to move negative(up)
+	if yDiff > 3 {
+		g.particles[0].yVel = -3
+	} else if yDiff < -3{
+		g.particles[0].yVel = 3
+	} else {
+		g.particles[0].yVel = 0
+	}
+
+	for i := range g.particles {
+		g.particles[i].Update()
 	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	for i := range g.particles {
-	vector.FillCircle(screen, g.particles[i].xPos, g.particles[i].yPos, g.particles[i].radius, g.particles[i].particleColor, true)
+		vector.FillCircle(screen, g.particles[i].xPos, g.particles[i].yPos, g.particles[i].radius, g.particles[i].particleColor, true)
 	}
 	ebitenutil.DebugPrint(
-	screen,
-	fmt.Sprintf("Particle 0: R %d, G %d, B %d\nParticle 1: R %d, G %d, B %d\nColor difference: %.0f", 
-	g.particles[0].particleColor.R, g.particles[0].particleColor.G, g.particles[0].particleColor.B,
-	g.particles[1].particleColor.R, g.particles[1].particleColor.G, g.particles[1].particleColor.B,
-	g.debugColorDifference),
-	
-)
+		screen,
+		fmt.Sprintf("Particle 0: R %d, G %d, B %d\nParticle 1: R %d, G %d, B %d\nColor difference: %.0f",
+			g.particles[0].particleColor.R, g.particles[0].particleColor.G, g.particles[0].particleColor.B,
+			g.particles[1].particleColor.R, g.particles[1].particleColor.G, g.particles[1].particleColor.B,
+			g.debugColorDifference),
+	)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -129,10 +98,14 @@ func main() {
 	ebiten.SetWindowSize(worldWidth, worldHeight)
 	ebiten.SetWindowTitle("Simland-Go")
 
-	
+	particles := createRedParticles(1)
+
+	particles = append(particles, createGreenParticles(1)...)
+
 	game := &Game{
-		particles: createParticles(2),
-		}
+		particles: particles,
+	}
+
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
---
+++ particle.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"image/color"
+	"math/rand/v2"
+)
+
+type Particle struct {
+	xPos          float32
+	yPos          float32
+	xVel          float32
+	yVel          float32
+	radius        float32
+	particleColor color.RGBA
+}
+
+const (
+	maxXVel = 10
+	maxYVel = 10
+)
+
+func (p *Particle) Update() {
+	p.xPos += p.xVel
+	p.yPos += p.yVel
+
+	//Wall Collision Detection
+	//Right Wall
+	if p.xPos+p.radius >= worldWidth {
+		p.xPos = worldWidth - p.radius
+		p.xVel *= -1
+	}
+	// Left Wall
+	if p.xPos <= p.radius {
+		p.xPos = p.radius
+		p.xVel *= -1
+	}
+	//Bottom wall
+	if p.yPos+p.radius >= worldHeight {
+		p.yPos = worldHeight - p.radius
+		p.yVel *= -1
+	}
+	//Top wall
+	if p.yPos <= p.radius {
+		p.yPos = p.radius
+		p.yVel *= -1
+	}
+}
+
+func particleDistance(p1XDist, p2XDist, p1YDist, p2YDist int) (xDiff int, yDiff int) {
+	xDiff = p1XDist - p2XDist
+	yDiff = p1YDist - p2YDist
+
+	return xDiff, yDiff
+}
+
+func createParticles(count int) []Particle {
+	particleList := make([]Particle, 0, count)
+	radius := 3
+
+	for range count {
+		particle := Particle{
+			xPos:          float32(rand.IntN(worldWidth-2*radius) + radius),
+			yPos:          float32(rand.IntN(worldHeight-2*radius) + radius),
+			xVel:          float32(rand.IntN(maxXVel) + 1), // Random velocity, (range max) + {Shift range upwards}
+			yVel:          float32(rand.IntN(maxYVel) + 1),
+			radius:        float32(radius),
+			particleColor: color.RGBA{uint8(rand.IntN(256)), uint8(rand.IntN(256)), uint8(rand.IntN(256)), 255},
+		}
+		particleList = append(particleList, particle)
+	}
+	return particleList
+}
+
+func createRedParticles(count int) []Particle {
+	particleList := make([]Particle, 0, count)
+	radius := 3
+
+	for range count {
+		particle := Particle{
+			xPos:          float32(rand.IntN(worldWidth-2*radius) + radius),
+			yPos:          float32(rand.IntN(worldHeight-2*radius) + radius),
+			xVel:          float32(3),
+			yVel:          float32(3),
+			radius:        float32(radius),
+			particleColor: color.RGBA{255, 0, 0, 255},
+		}
+		particleList = append(particleList, particle)
+	}
+	return particleList
+}
+func createGreenParticles(count int) []Particle {
+	particleList := make([]Particle, 0, count)
+	radius := 3
+
+	for range count {
+		particle := Particle{
+			xPos:          float32(rand.IntN(worldWidth-2*radius) + radius),
+			yPos:          float32(rand.IntN(worldHeight-2*radius) + radius),
+			xVel:          float32(2),
+			yVel:          float32(2),
+			radius:        float32(radius),
+			particleColor: color.RGBA{0, 255, 0, 255},
+		}
+		particleList = append(particleList, particle)
+	}
+	return particleList
+}
+func createBlueParticles(count int) []Particle {
+	particleList := make([]Particle, 0, count)
+	radius := 3
+
+	for range count {
+		particle := Particle{
+			xPos:          float32(rand.IntN(worldWidth-2*radius) + radius),
+			yPos:          float32(rand.IntN(worldHeight-2*radius) + radius),
+			xVel:          float32(rand.IntN(maxXVel) + 1),
+			yVel:          float32(rand.IntN(maxYVel) + 1),
+			radius:        float32(radius),
+			particleColor: color.RGBA{0, 0, 255, 255},
+		}
+		particleList = append(particleList, particle)
+	}
+	return particleList
+}
